372c6a2c19ec0664a40ca3f7ddedeef5ae12f035
Oscar Sanchez <oscar_sanchez39@hotmail.com>
2016-06-24 23:37:47 -0500

    ejercicio de Juego de PingPong.

--- pingpong/main.js
+++ pingpong/main.js
@@ -27,22 +27,50 @@
 	}
 
 	self.BoardView.prototype = {
+		clean: function() {
+			this.ctx.clearRect(0,0, this.board.width, this.board.height);
+		},
 		draw: function() {
 			for (var i = this.board.elements.length - 1; i >= 0; i--) {
 				var el = this.board.elements[i];
 				draw(this.ctx, el);
 			}
+		},
+		play: function() {
+			this.clean();
+			this.draw();
 		}
 	}
 
 	function draw(ctx, element) {
-		if (element !== null && element.hasOwnProperty('kind')) {
+		// if (element !== null && element.hasOwnProperty('kind')) {
 			switch(element.kind){
 				case "rectangle":
 					ctx.fillRect(element.x,element.y,element.width,element.height);
 					break;
+				case "circle":
+					ctx.beginPath();
+					ctx.arc(element.x,element.y,element.radius,0,7);
+					ctx.fill();
+					ctx.closePath();
+					break;
 			}
-		}
+		// }
+	}
+})();
+
+(function() {
+	self.Ball = function(x,y,radius,board) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.board = board;
+		this.speed_y = 0;
+		this.speed_x = 3;
+
+		this.board.ball = this;
+		this.kind = 'circle';
+
 	}
 })();
 
@@ -55,24 +83,42 @@
 		this.board = board;
 		this.board.bars.push(this);
 		this.kind = 'rectangle';
+		this.speed = 10;
 	}
 	self.Bar.prototype = {
 		down: function() {
-			// body...
+			this.y += this.speed;
 		},
 		up: function() {
-			// body...
+			this.y -= this.speed;
 		}
 	}
 })();
 
-window.addEventListener("load", main);
+var board = new Board(800,400);
+var bar = new Bar(0,150,40,100, board);
+var bar2 = new Bar(760,150,40,100, board);
+var ball = new Ball(350,100,10,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
 
-function main() {
-	var board = new Board(800,400);
-	var bar = new Bar(0,150,40,100, board);
-	var bar2 = new Bar(760,150,40,100, board);
-	var canvas = document.getElementById('canvas');
-	var board_view = new BoardView(canvas, board);
-	board_view.draw();
+document.addEventListener("keydown", function(ev) {
+	ev.preventDefault();
+	if (ev.keyCode == 38) {
+		bar2.up();
+	} else if (ev.keyCode == 40){
+		bar2.down();
+	} else if (ev.keyCode == 87){
+		bar.up();
+	} else if (ev.keyCode == 83){
+		bar.down();
+	}
+})
+
+window.requestAnimationFrame(Controller);
+// window.addEventListener("load", Controller);
+
+function Controller() {
+	board_view.play();
+	window.requestAnimationFrame(Controller);
 }
