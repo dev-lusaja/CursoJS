3171deeac90e4ab87eb4ba823da0c31ad6ff7cd9
Oscar Sanchez <oscar_sanchez39@hotmail.com>
2016-06-30 17:00:43 -0500

    Se adiciono movimiento de pelota y barra espaciadora para el pause.

--- pingpong/main.js
+++ pingpong/main.js
@@ -10,7 +10,8 @@
 
 	self.Board.prototype = {
 		get elements(){
-			var elements = this.bars;
+			// var elements = this.bars;
+			var elements = this.bars.map(function(bar) {return bar;});
 			elements.push(this.ball);
 			return elements;
 		}
@@ -37,8 +38,11 @@
 			}
 		},
 		play: function() {
-			this.clean();
-			this.draw();
+			if (this.board.playing) {
+				this.clean();
+				this.draw();
+				this.board.ball.move();
+			}
 		}
 	}
 
@@ -67,10 +71,18 @@
 		this.board = board;
 		this.speed_y = 0;
 		this.speed_x = 3;
+		this.direction = 1;
 
 		this.board.ball = this;
 		this.kind = 'circle';
 
+	}
+
+	self.Ball.prototype = {
+		move: function () {
+			this.x += (this.speed_x * this.direction);
+			this.y += (this.speed_y);
+		}
 	}
 })();
 
@@ -103,19 +115,31 @@ var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
 
 document.addEventListener("keydown", function(ev) {
-	ev.preventDefault();
+	
 	if (ev.keyCode == 38) {
+		ev.preventDefault();
 		bar2.up();
 	} else if (ev.keyCode == 40){
+		ev.preventDefault();
 		bar2.down();
 	} else if (ev.keyCode == 87){
+		ev.preventDefault();
 		bar.up();
 	} else if (ev.keyCode == 83){
+		ev.preventDefault();
 		bar.down();
+	} else if (ev.keyCode == 32){
+		ev.preventDefault();
+		board.playing = !board.playing;
 	}
 })
 
+board_view.draw();
+
 window.requestAnimationFrame(Controller);
+setTimeout(function() {
+	ball.direction = -1;
+}, 4000);
 // window.addEventListener("load", Controller);
 
 function Controller() {
